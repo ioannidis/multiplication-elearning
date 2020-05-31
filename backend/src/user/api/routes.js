b1f109593ee331289be08afef0b6d73b933cefb7
ioannidis <panos277@hotmail.com>
2020-05-31 20:01:56 +0300

    Update users

--- backend/src/user/api/routes.js
+++ backend/src/user/api/routes.js
@@ -1,10 +1,29 @@
 const express   = require('express');
+const jwt       = require('jsonwebtoken');
 
 const router            = express.Router();
-const userService     = require('../services/user-service');
+const achievementRouter = require('../../achievements/api/routes')
+const userService       = require('../services/user-service');
+const roleMiddleware = require('../../shared/has-role-middleware');
 
 
-router.get('/', async (req, res, next) => {
+router.get('/me', async (req, res, next) => {
+    try {
+        const tokenPayload = jwt.decode(req.headers['authorization'].slice(7)).principal;
+        const users = await userService.findOne({username: tokenPayload.username});
+
+        if (!users)
+            return res.status(404).json({error: {status: 404, message: 'Not found'}});
+        users.password = '****'
+        return res.json({users});
+
+    } catch (err) {
+        return res.status(500).json({error: err});
+    }
+})
+
+
+router.get('/', [roleMiddleware.hasRole(['teacher'])], async (req, res, next) => {
     try {
         const users = await userService.find({});
 
@@ -17,9 +36,9 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:username', [roleMiddleware.hasRole(['teacher'])], async (req, res, next) => {
     try {
-        const user = await userService.findOne({id: req.params.id});
+        const user = await userService.findOne({username: req.params.username});
 
         if (!user)
             return res.status(404).json({error: {status: 404, message: 'Not found'}});
@@ -30,6 +49,6 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
-
+router.use('/:username/achievements', (req, res, next) => {req.username = req.params.username; next();}, achievementRouter)
 
 module.exports = router;
