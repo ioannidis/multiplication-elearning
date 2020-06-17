f6df2b698498d08499a9cd3470fa0ae8b142b2aa
ioannidis <panos277@hotmail.com>
2020-06-17 20:10:42 +0300

    Update app routing module

--- frontend/mlt-elearning/src/app/app-routing.module.ts
+++ frontend/mlt-elearning/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {AuthGuard} from "./core/guards/auth-guard.service";
 const routes: Routes = [
   {path: '', canActivate: [AuthGuard], pathMatch: 'full', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)},
   {path: 'lessons', canActivate: [AuthGuard], loadChildren: () => import('./modules/lessons/lessons.module').then(m => m.LessonsModule)},
+  {path: 'reviews', canActivate: [AuthGuard], loadChildren: () => import('./modules/review/review.module').then(m => m.ReviewModule)},
   {path: 'auth', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)},
   {path: 'error', loadChildren: () => import('./core/error/error.module').then(m => m.ErrorModule)},
   {path: '404', component: NotFoundComponent, data: { error: 404, message: 'Oops Page Not Found', description: 'The page you are looking for does not exist or has been moved.'}},
