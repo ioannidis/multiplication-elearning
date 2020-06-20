3690a3718cc18b49d2d19f5c04ba8bd3be258f7e
ioannidis <panos277@hotmail.com>
2020-06-20 15:52:47 +0300

    Update user repo

--- backend/src/user/repositories/user-repository.js
+++ backend/src/user/repositories/user-repository.js
@@ -28,10 +28,41 @@ const save = async (data) => {
     } catch (err) {
         return err;
     }
-}
+};
+
+const findOneAndUpdate = async (data) => {
+    try {
+        const user = new User(data);
+        console.log(data);
+        console.log("==========")
+        console.log(user)
+        console.log("===========")
+        console.log("ANTE GAMISOU")
+        // await user.updateOne({_id: user._id}, user, () => {});
+        const a = await User.updateOne({username: data.user.username}, {password: data.password})
+        console.log(a)
+        return {data: {user}};
+    } catch (err) {
+        return err;
+    }
+};
 
 module.exports = {
     find,
     findOne,
+    findOneAndUpdate,
     save
 };
+
+
+//{
+//     "_id" : ObjectId("5ecfde664ece471958666bb0"),
+//     "enabled" : true,
+//     "username" : "klanos",
+//     "password" : "$2b$10$bOMnGrOaedz.EPQPPJ7Dje4bKXf0WB7lWvWqMkPBz6KtkCHZpM/T6",
+//     "email" : "klanos@gmail.comg",
+//     "role" : "student",
+//     "firstName" : "Klanos",
+//     "lastName" : "Klanidis",
+//     "__v" : 0
+// }
