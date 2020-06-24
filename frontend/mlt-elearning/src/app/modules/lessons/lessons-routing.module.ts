9d14fa2e5451317d47cc6d354676219921456e2e
ioannidis <panos277@hotmail.com>
2020-06-25 01:14:40 +0300

    Update lessons module

--- frontend/mlt-elearning/src/app/modules/lessons/lessons-routing.module.ts
+++ frontend/mlt-elearning/src/app/modules/lessons/lessons-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LessonsComponent} from "./lessons/lessons.component";
 import {LessonsShowComponent} from "./lessons-show/lessons-show.component";
+import {LessonsAdminComponent} from "../admin/lessons-admin/lessons-admin.component";
 
 const routes: Routes = [
   {
     path: '', component: LessonsComponent, children: [
-      {path: ':url', component: LessonsShowComponent}
+      {path: ':url', component: LessonsShowComponent},
+      {path: 'lessons', component: LessonsAdminComponent}
     ]
   }
 ];
